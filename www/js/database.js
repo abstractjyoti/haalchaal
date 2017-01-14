44adecdf21e1f9f0bcf6eb9e2a9ae6863377ab41
abstractjyoti <abstract_jyoti@rediffmail.com>
2017-01-14 20:44:50 +0530

    new app

---
+++ www/js/database.js
@@ -0,0 +1,35 @@
+var db = openDatabase('fettle_fling', '1.0', 'Test DB', 2 * 1024 * 1024);
+
+db.transaction(function (tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS `users` (id integer primary key AUTOINCREMENT, name varchar,username varchar,gender varchar,age number)');
+    // tx.executeSql('drop table users');
+});
+
+angular.module('fettleflingdb', ['ngCordova'])
+    .factory('MyDatabase', function ($cordovaToast) {
+        return {
+            insertUser: function (userinfo) {
+                db.transaction(function (tx) {
+                    tx.executeSql('INSERT INTO `users` ( name,username,gender,age) VALUES ("' + userinfo.firstname + " " + userinfo.lastname + '","' + userinfo.username + '","' + userinfo.gender + '",' + userinfo.age + ')');
+
+                });
+            },
+
+            checkLogin: function (username) {
+                db.transaction(function (tx) {
+                    tx.executeSql('SELECT * FROM `users` where username="' + username + '"', [], function (tx, results) {
+                        if (results.rows.length == 1) {
+                            $cordovaToast.showLongBottom('You are logged in !').then(function (success) {
+                                // success
+                            }, function (error) {
+                                // error
+                            });
+
+                        }
+
+                    }, null);
+                });
+
+            }
+        }
+    });
