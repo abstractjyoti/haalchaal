3db76ddb28ac73eea6078925192e12b27c5097e5
abstractjyoti <abstract_jyoti@rediffmail.com>
2017-03-17 21:08:24 +0530

    displaying questions of personality test

--- www/js/database.js
+++ www/js/database.js
@@ -4,7 +4,7 @@ db.transaction(function (tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS `users` (id integer primary key AUTOINCREMENT, name varchar,username varchar,gender varchar,age number)');
     // tx.executeSql('drop table users');
     tx.executeSql('CREATE TABLE IF NOT EXISTS `questioncategory` (id integer primary key, catname varchar,parentid integer)');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS `questions` (id integer primary key AUTOINCREMENT, question varchar,option1 varchar,option2 varchar,option3 varchar,option4 varchar,option5 varchar,correctanswer varchar,explanation varchar,catid integer)');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS `questions` (id integer primary key AUTOINCREMENT, question varchar,option1 varchar,option2 varchar,option3 varchar,option4 varchar,option5 varchar,correctanswer varchar,explanation varchar,catid integer,questiontype varchar)');
     // tx.executeSql('drop table users');
 });
 db.transaction(function (tx) {
@@ -17,7 +17,7 @@ db.transaction(function (tx) {
     tx.executeSql("insert into questioncategory values(6,'analogy',3)");
     tx.executeSql("insert into questioncategory values(7,'classification',3)");
     tx.executeSql("insert into questioncategory values(8,'logic',3)");
-    tx.executeSql("insert into questioncategory values(9,'personality',3)");
+    tx.executeSql("insert into questioncategory values(9,'personality',0)");
     //QUESTION TABLE DATA
     tx.executeSql("insert into questions(question,option1,option2,option3,option4,option5,correctanswer,catid) values('EXPLORE : DISCOVER','read : skim','research : learn','write : print','think : relate','sleep : wake','research : learn',6)");
     tx.executeSql("insert into questions(question,option1,option2,option3,option4,option5,correctanswer,catid) values('COBBLER : SHOE','jockey : horse','contractor : building','mason : stone','cowboy : boot','potter : paint','contractor : building',6)");
@@ -37,14 +37,18 @@ db.transaction(function (tx) {
     tx.executeSql("insert into questions(question,option1,option2,option3,option4,option5,correctanswer,catid) values(' Which number should come next in the series<br>1, 3, 6, 10, 15,','8','11','24','21','27','21',8)");
     tx.executeSql("insert into questions(question,option1,option2,option3,option4,option5,correctanswer,catid) values('165135 is to peace as 1215225 is to','lead','love ','loop','castle',null,'love',8)");
     tx.executeSql("insert into questions(question,option1,option2,option3,option4,option5,correctanswer,catid) values('Library is to book as book is to','page','copy ','binding','cover',null,'page',8)");
-    tx.executeSql("insert into questions(img src="img/1.jpg") values ('What you see first', 'face=1','apple=2','person sitting=3',null,null,9));
+    
+    
+    
+    //Personality
+    tx.executeSql("insert into questions(question,option1 ,option2 ,option3,catid,questiontype ) values ('img/1.jpg', 'face,1','apple,2','person sitting,3',9,'img')");
                   
-    tx.executeSql("insert into questions(img src="img/2.jpg") values ('What you see first', 'car=1','Man with binocular=2','letter A=3',null,null,9'));
-    tx.executeSql("insert into questions(img src="img/3.jpg") values ('What you see first', 'Bowling pins=1','footprints=2','nesting dolls=3',null,null,9'));
-    tx.executeSql("insert into questions(img src="img/4.jpg") values ('What you see first', 'apple=1','butterfly=2','knife=3',null,null,9'));
-    tx.executeSql("insert into questions(img src="img/5.jpg") values ('What you see first', 'face=1','dog=2','precipice=3',null,null,9'));
-    tx.executeSql("insert into questions(img src="img/6.jpg") values ('What you see first', 'crocodile=1','mountains and water=2','people in boat=3',null,null,9'));
-    tx.executeSql("insert into questions(img src="img/7.jpg") values ('What you see first', 'whale=1','moon and light on water=2','a person surfing=3',null,null,9'));
+    tx.executeSql("insert into questions(question,option1 ,option2 ,option3,catid,questiontype ) values ('img/2.jpg', 'car,1','Man with binocular,2','letter A,3',9,'img')");
+    tx.executeSql("insert into questions(question,option1 ,option2 ,option3,catid,questiontype ) values ('img/3.jpg', 'Bowling pins,1','footprints,2','nesting dolls,3',9,'img')");
+    tx.executeSql("insert into questions(question,option1 ,option2 ,option3,catid,questiontype ) values ('img/4.jpg', 'apple,1','butterfly,2','knife,3',9,'img')");
+    tx.executeSql("insert into questions(question,option1 ,option2 ,option3,catid,questiontype ) values ('img/5.jpg', 'face,1','dog,2','precipice,3',9,'img')");
+    tx.executeSql("insert into questions(question,option1 ,option2 ,option3,catid,questiontype ) values ('img/6.jpg', 'crocodile,1','mountains and water,2','people in boat,3',9,'img')");
+    tx.executeSql("insert into questions(question,option1 ,option2 ,option3,catid,questiontype) values ('img/7.jpg', 'whale,1','moon and light on water,2','a person surfing,3',9,'img')");
                   
     // tx.executeSql('drop table users');
 });
