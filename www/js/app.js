0f04064cff10c92d0ec0ff93e89a6add692cc8a2
abstractjyoti <abstract_jyoti@rediffmail.com>
2017-04-06 16:08:34 +0530

    oops

--- www/js/app.js
+++ www/js/app.js
@@ -99,5 +99,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'fettleflingdb', 'ngC
         }
     });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/playlists');
+    $urlRouterProvider.otherwise('/app/login');
 });
